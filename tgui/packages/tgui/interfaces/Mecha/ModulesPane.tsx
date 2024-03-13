bce907ee0b7e52d4563f8a8ace55f6b62062bccd
SyncIt21 <110812394+SyncIt21@users.noreply.github.com>
2024-03-14 01:00:52 +0530

    Typescript refactor for Number Input (#81913)
    ## About The Pull Request
    - Fixes #80971
    - Fixes #81379
    
    You now only click once to highlight & edit the value. Converted
    NumberInput to typescript
    
    ## Changelog
    :cl:
    fix: NumberInput(used in chem heater, plumbing reaction chamber etc)
    highlighting & editing requires only single click like before
    refactor: Typescript conversion for NumberInput TGUI Component
    /:cl:
    
    ---------
    
    Co-authored-by: Jeremiah <42397676+jlsnow301@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/Mecha/ModulesPane.tsx
+++ tgui/packages/tgui/interfaces/Mecha/ModulesPane.tsx
@@ -544,7 +544,7 @@ const SnowflakeRadio = (props) => {
       </LabeledList.Item>
       <LabeledList.Item label="Frequency">
         <NumberInput
-          animate
+          animated
           unit="kHz"
           step={0.2}
           stepPixelSize={10}
@@ -737,7 +737,7 @@ const SnowflakeAirTank = (props) => {
             minValue={tank_pump_pressure_min}
             maxValue={tank_pump_pressure_max}
             step={10}
-            format={(value) => Math.round(value)}
+            format={(value) => `${Math.round(value)}`}
             onChange={(e, value) =>
               act('equip_act', {
                 ref: ref,
